300f5600378d50b8ff0ead0125c7d56de969881d
Debasish Ghosh <ghosh.debasish@gmail.com>
2021-11-19 20:57:20 +0530

    Added in memory repositories for Account and Balance

---
+++ modules/core/src/main/scala/tradex/domain/repository/inmemory/AccountRepositoryInMemory.scala
@@ -0,0 +1,28 @@
+package tradex.domain
+package repository.inmemory
+
+import zio._
+import java.time._
+import collection.immutable.Map
+import repository.AccountRepository
+import model.account._
+
+case class AccountRepositoryInMemory(state: Ref[Map[AccountNo, Account]]) extends AccountRepository {
+
+  override def queryByAccountNo(no: AccountNo): Task[Option[Account]] = state.get.map(_.get(no))
+
+  override def store(a: Account): Task[Account] = state.update(m => m + ((a.no, a))).map(_ => a)
+
+  override def store(as: List[Account]): Task[Unit] = state.update(m => m ++ (as.map(a => (a.no, a))))
+
+  override def allOpenedOn(openedOnDate: LocalDate): Task[List[Account]] =
+    state.get.map(_.values.filter(_.dateOfOpen == openedOnDate).toList)
+
+  override def all: Task[List[Account]] = state.get.map(_.values.toList)
+
+  override def allClosed(closeDate: Option[LocalDate]): Task[List[Account]] =
+    state.get.map(_.values.filter(_.dateOfClose.isDefined).toList)
+
+  override def allAccountsOfType(accountType: AccountType): Task[List[Account]] =
+    state.get.map(_.values.filter(_.accountType == accountType).toList)
+}
