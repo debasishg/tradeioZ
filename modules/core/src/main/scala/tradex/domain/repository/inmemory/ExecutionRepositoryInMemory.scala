31b88075f3664bf0f8723bd1c699aa73e133386f
Debasish Ghosh <ghosh.debasish@gmail.com>
2021-11-19 21:11:52 +0530

    Added in memory repositories for execution and instrument

---
+++ modules/core/src/main/scala/tradex/domain/repository/inmemory/ExecutionRepositoryInMemory.scala
@@ -0,0 +1,15 @@
+package tradex.domain
+package repository.inmemory
+
+import zio._
+import zio.prelude.NonEmptyList
+import model.execution._
+import repository.ExecutionRepository
+
+final case class ExecutionRepositoryInMemory(state: Ref[Map[ExecutionReferenceNo, Execution]])
+    extends ExecutionRepository {
+  def store(exe: Execution): Task[Execution] = state.update(m => m + ((exe.executionRefNo.get, exe))).map(_ => exe)
+
+  def storeMany(executions: NonEmptyList[Execution]): Task[Unit] =
+    state.update(m => m ++ (executions.map(exe => (exe.executionRefNo.get, exe))))
+}
