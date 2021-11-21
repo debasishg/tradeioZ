e16f144a467debf7db75d2795cb8485b60191184
Debasish Ghosh <ghosh.debasish@gmail.com>
2021-11-21 22:53:17 +0530

    Now using zio clock

--- modules/tests/src/test/scala/tradex/domain/TradingServiceSpec.scala
+++ modules/tests/src/test/scala/tradex/domain/TradingServiceSpec.scala
@@ -3,6 +3,8 @@ package tradex.domain
 import zio._
 import zio.test._
 import zio.test.Assertion._
+import zio.test.environment.TestClock
+import zio.duration._
 
 import generators._
 import repository._
@@ -12,16 +14,19 @@ import java.time._
 
 object TradingServiceSpec extends DefaultRunnableSpec {
   val localDateZERO = LocalDateTime.ofEpochSecond(0, 0, ZoneOffset.UTC)
-  val spec = suite("Trading and Accounting Service")(
-    testM("successfully invoke a service") {
+  val spec = suite("Trading Service")(
+    testM("successfully invoke getAccountsOpenedOn") {
       checkM(Gen.listOfN(5)(accountGen)) { accounts =>
         for {
+          _   <- TestClock.adjust(1.day)
+          now <- clock.instant
+          dt = LocalDateTime.ofInstant(now, ZoneOffset.UTC)
           _ <- AccountRepository.store(
-            accounts.map(_.copy(dateOfOpen = localDateZERO))
+            accounts.map(_.copy(dateOfOpen = dt))
           )
-          fetched <- TradingService.getAccountsOpenedOn(localDateZERO.toLocalDate())
+          fetched <- TradingService.getAccountsOpenedOn(dt.toLocalDate())
         } yield assert(
-          fetched.forall(_.dateOfOpen.toLocalDate() == localDateZERO.toLocalDate())
+          fetched.forall(_.dateOfOpen.toLocalDate() == dt.toLocalDate())
         )(
           equalTo(true)
         )
